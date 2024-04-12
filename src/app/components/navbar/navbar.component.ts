758308ca3e644b244a76621e10d1aaffa2710e99
ukalto <maxi.gaber@gmail.com>
2024-04-12 21:26:17 +0200

    added dark mode, menu bar, finished

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Inject, Renderer2} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(private renderer: Renderer2, @Inject(DOCUMENT) private document: Document) { }
+
+  homeRef = "index.html";
+  routes = [["index.html", "Home"], ["about.html", "About"], ["services.html", "Services"]];
+  isHamburgerMenuOpen = false;
+  isLightModeOn = true;
+
+  toggleHamburgerMenu() {
+    this.isHamburgerMenuOpen = !this.isHamburgerMenuOpen;
+  }
+
+  toggleLightMode() {
+    this.isLightModeOn = !this.isLightModeOn;
+    if (this.isLightModeOn) {
+      this.renderer.removeClass(this.document.body, 'dark-mode');
+    } else {
+      this.renderer.addClass(this.document.body, 'dark-mode');
+    }
+  }
+
+  goHome() {
+    window.location.href = this.homeRef;
+  }
 }
